9f7dc4c8aab4615424ead6d70bbd39158ed5e96a
markisgod92 <markisgod92@gmail.com>
2024-09-06 19:57:39 +0200

    index ok

--- js/index.js
+++ js/index.js
@@ -7,6 +7,11 @@ const playlistVisualizer = new Visualize(playlistContainer);
 const heroImg = document.getElementById("heroImg");
 const heroSection = document.getElementById("heroSection");
 const player = document.querySelector("audio");
+const playerImg = document.getElementById("playerImg");
+const playerData = document.getElementById("playerData");
+const playBtn = document.getElementById("playBtn");
+const progressBar = document.getElementById("progress-bar");
+let isPlaying = false;
 
 window.addEventListener("DOMContentLoaded", async () => {
     await Promise.all([fillSongs(), fillsPlaylists(), fillHero()])
@@ -57,7 +62,37 @@ const writeHero = (data) => {
     heroSection.querySelector("h3").innerText = data.artist.name;
     heroSection.querySelector("p").innerText = `Ascolta il nuovo singolo di ${data.artist.name}!`;
     
+    
+    player.src = data.preview;
+    playerImg.src = data.album.cover;
+    playerImg.alt = data.album.title;
+
+    playerData.querySelector("h5").innerText = data.title;
+    playerData.querySelector("h6").innerText = data.artist.name;
+
     heroSection.querySelector("div > button:first-of-type").addEventListener("click", () => {
-        player.src = data.preview;
+        player.play();
+        isPlaying = true;
     })
 }
+
+playBtn.addEventListener("click", () => {
+    isPlaying ? player.pause() : player.play()
+    isPlaying = !isPlaying;
+})
+
+player.addEventListener("timeupdate", () => {
+    const currentTime = player.currentTime;
+    const duration = player.duration;
+
+    const currentMinutes = Math.floor(currentTime / 60);
+    const currentSeconds = Math.floor(currentTime % 60);
+    const totalMinutes = Math.floor(duration / 60);
+    const totalSeconds = Math.floor(duration % 60);
+
+    /* currentTimeDisplay.textContent = `${currentMinutes}:${currentSeconds < 10 ? '0' : ''}${currentSeconds}`;
+    totalTimeDisplay.textContent = `${totalMinutes}:${totalSeconds < 10 ? '0' : ''}${totalSeconds}`; */
+
+    const progress = (currentTime / duration) * 100;
+    progressBar.style.width = `${progress}%`;
+});
